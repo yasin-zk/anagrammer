b4564c85de2f7ff37683f6f55473e5681f607e01
yzk-home <yasin@compassfairs.no>
2023-02-21 11:43:04 +0100

    finish OOP class later

--- src/anagram_class.py
+++ src/anagram_class.py
@@ -7,4 +7,17 @@ class Anagrams:
         self.anagrams = collections.defaultdict(list)
     
     def read_file(self):
-        
+        with open(self.filename, 'r') as fil:
+            reader = csv.reader(fil)
+            for row in reader:
+                for word in row:
+                    pass
+
+    def add_word(self, word):
+        sorted_word = ''.join(sorted(word.strip().lower()))
+        self.anagrams[sorted_word].append(word)
+
+    def get_anagrams(self):
+        pass
+
+    
