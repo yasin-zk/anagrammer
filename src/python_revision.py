b013923a97f0588395c11044f45699c188ffd9a8
yzk-home <yasin@compassfairs.no>
2023-02-21 11:21:06 +0100

    fix anagram and and add loop

--- src/python_revision.py
+++ src/python_revision.py
@@ -1,5 +1,3 @@
-# this is a comment
-
 def isAnagram():
     '''
     (word1) - > input
@@ -7,16 +5,30 @@ def isAnagram():
     Check if word is anagram
     '''
 
-    word1 = input('Anagram 1 : ')
-    word2 = input('Anagram 2 : ')
-    if(len(word1) != len(word2)):
+    word1 = input('Anagram 1: ')
+    word2 = input('Anagram 2: ')
+    if len(word1) != len(word2):
         return 'Words are not equal length' # throw error message The words are not equal length.
-    
+
     for char in word1:
         if char not in word2:
-            return 'Not anagram' #False
-        return 'is Anagram' #True
+            return 'Not anagram' # False
+        # remove the character from word2 so it's not matched again
+        word2 = word2.replace(char, '', 1)
 
-#expected word1 and word2 input : "silent", "listen" returns "is Anagram"
-print(isAnagram())
+    # make sure all characters in word1 have been matched
+    if len(word2) == 0:
+        return 'Is Anagram' # True
+    else:
+        return 'Not anagram' # False
+
+
+while True:
+    result = isAnagram()
+    print(result)
+    if result == "Not anagram":
+        continue
+    leaveloop = input("Exit? (Yes/No) ")
+    if leaveloop.upper() == "YES":
+        break
 
